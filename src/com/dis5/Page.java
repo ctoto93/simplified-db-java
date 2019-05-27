1ff2cb044e59b59d71e7b5774c2155a1700e5247
Christoper <christoper@go-jek.com>
2019-05-27 19:44:29 +0200

    Page constructor to accept all fields

--- src/com/dis5/Page.java
+++ src/com/dis5/Page.java
@@ -5,6 +5,12 @@ public class Page {
     private int lsnId;
     private String data;
 
+    public Page(int id, int lsnId, String data) {
+        this.id = id;
+        this.lsnId = lsnId;
+        this.data = data;
+    }
+
     public int getId() {
         return id;
     }
